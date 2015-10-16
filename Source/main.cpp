b769c79b6639d039946b90c37d4f8db931091c55
Rivero Sato <riveroyuichitrabajosdeinforma@gmail.com>
2015-10-16 23:18:32 +0900

    Pond Demo
    Pond Demo Build

--- Source/main.cpp
+++ Source/main.cpp
@@ -24,21 +24,44 @@ public:
 	void OnMouseDown(WPARAM btnState, int x, int y) override;
 	void OnMouseUp(WPARAM btnState, int x, int y) override;
 	void OnMouseMove(WPARAM btnState, int x, int y) override;
+
+	bool SceneInit();
 private:
 	Camera m_camera;
 	Model m_shapes;
-	LightShader m_colorShader;
-	LightShader m_lightShader;
-	LightShader m_boxShader;
-	LightShader m_cylinderShader;
-	LightShader m_sphereShader;
-	
-	ID3D11ShaderResourceView* m_crateTexture;
-	ID3D11ShaderResourceView* m_grassTexture;
-	DirectX::XMMATRIX m_defTexTranf;
-	DirectX::XMMATRIX m_grassTexTransf;
-	float m_grassX;
-	float m_grassY;
+
+	ID3D11ShaderResourceView* m_texCrate;
+	ID3D11ShaderResourceView* m_texGrass;
+	ID3D11ShaderResourceView* m_texWater;
+	ID3D11ShaderResourceView* m_texBrick;
+	DirectX::XMMATRIX m_texTranfDef;
+	DirectX::XMMATRIX m_texTranfCrate;
+	DirectX::XMMATRIX m_texTransfGrass;
+	DirectX::XMMATRIX m_texTransfWater;
+	DirectX::XMMATRIX m_texTransfBrick;
+
+	//Light
+	BasicLight m_basicLight;
+
+	//Materials
+	Material m_matShiny;
+	Material m_matRough;
+	Material m_matWater;
+
+	//Renderers
+	LightShader m_rendererShiny;
+	LightShader m_rendererRough;
+	LightShader m_rendererWater;
+
+	//Geometry
+	offsetData m_offsetPlain;
+	DirectX::XMMATRIX m_worldPlain;
+
+	offsetData m_offsetCont[5];
+	DirectX::XMMATRIX m_worldCont[5];
+
+	offsetData m_offsetWater;
+	DirectX::XMMATRIX m_worldWater;
 
 	float m_radius;
 	float m_phi;
@@ -50,44 +73,11 @@ private:
 	float m_cameraZVel;
 	float m_radiusVel;
 	float m_movementAngle;
-	float m_skullAngle;
 	XMFLOAT3 m_pointBulbPos;
+	float m_waterX;
+	float m_waterY;
 
 	POINT m_lastMousePos;
-
-	//Light
-	BasicLight m_basicLight;
-
-	//Materials
-	Material m_simpleMaterial;
-
-	//plain data
-	DirectX::XMMATRIX m_plainWorld;
-	offsetData m_plainOffset;
-	Material m_plainMaterial;
-
-	//box data
-	DirectX::XMMATRIX m_boxWorld;
-	offsetData m_boxOffset;
-	Material m_boxMaterial;
-
-	//Cylinders data
-	DirectX::XMMATRIX m_cylinderWorld[10];
-	offsetData m_cylinderOffset[10];
-	Material m_cylinderMaterial;
-
-	//spheres data
-	DirectX::XMMATRIX m_spheresWorld[11];
-	offsetData m_spheresOffset[11];
-	Material m_sphereMaterial;
-
-	//center sphere data
-	DirectX::XMMATRIX m_sphereWorld;
-	offsetData m_sphereOffset;
-
-	//skull data
-	DirectX::XMMATRIX m_skullWorld;
-	offsetData m_skullOffset;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
@@ -116,97 +106,59 @@ SimpleApp::SimpleApp(HINSTANCE instance, int width, int height)
 	m_cameraXVel(0),
 	m_cameraZVel(0),
 	m_radiusVel(0),
-	m_movementAngle(0),
-	m_skullAngle(0)
+	m_movementAngle(0)
 {
 }
 
 SimpleApp::~SimpleApp()
 {
-	m_colorShader.Shutdown();
-	m_lightShader.Shutdown();
+	m_rendererWater.Shutdown();
+	m_rendererWater.Shutdown();
+	m_rendererShiny.Shutdown();
 	m_shapes.Shutdown();
 }
 
-bool SimpleApp::Init()
+bool SimpleApp::SceneInit()
 {
-	if (!BaseApp::Init())
-	{
-		return false;
-	}
-	//additional init
-
+	//Vertor based Variables
 	m_cameraX = 0.0f;
-	m_cameraY = 7.0f;
-	m_cameraZ = -13.0f;
+	m_cameraY = 8.0f;
+	m_cameraZ = -15.0f;
 	m_radius = 25.0f;
 	m_theta = -14.0f;
 	m_cameraXVel = 5.0f;
 	m_cameraZVel = 5.0f;
 	m_radiusVel = 28.8f;
 	m_movementAngle = 5.0f;
-	m_skullAngle = 5.0f;
 	m_pointBulbPos.x = 0.0f;
 	m_pointBulbPos.y = 5.0f;
 	m_pointBulbPos.z = 5.0f;
 
-	m_defTexTranf = XMMatrixIdentity();
-	m_grassX = 1.0f;
-	m_grassY = 1.0f;
-	m_grassTexTransf = XMMatrixMultiply(m_defTexTranf, XMMatrixScaling(5.0f, 5.0f, 5.0f));
+	m_waterX = 1.0f;
+	m_waterY = 1.0f;
 
-	m_crateTexture = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/WoodCrate01.dds");
-	m_grassTexture = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/water1.dds");
+	//-----------------------------------------------------------------------------------------------------
+	//        Texture Init
+	//-----------------------------------------------------------------------------------------------------
+	m_texTranfDef = XMMatrixIdentity();
+	m_texTransfGrass = XMMatrixMultiply(m_texTranfDef, XMMatrixScaling(50.0f, 50.0f, 1.0f));
+	m_texTranfCrate = m_texTranfDef;
+	m_texTransfBrick = XMMatrixMultiply(m_texTranfDef, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	m_texTransfWater = XMMatrixMultiply(m_texTranfDef, XMMatrixScaling(1.0f, 1.0f, 1.0f));
 
-	m_plainOffset = m_shapes.AddGeometry(MODEL_TYPE_PLAIN);
-	m_plainWorld = XMMatrixIdentity();
+	m_texCrate = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/WoodCrate01.dds");
+	m_texWater = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/water2.dds");
+	m_texGrass = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/grass.dds");
+	m_texBrick = TextureLoader::CreateDDSTexture(m_d3dDevice, L"Resources/Textures/darkbrickdxt1.dds");
 
-	m_spheresOffset[10] = m_shapes.AddGeometry(MODEL_TYPE_GEOSPHERE);
-	XMMATRIX plbScale = XMMatrixScaling(0.5f, 0.5f, 0.5f);
-	XMMATRIX plbPos = XMMatrixTranslation(m_pointBulbPos.x, m_pointBulbPos.y, m_pointBulbPos.z);
-	m_spheresWorld[10] = XMMatrixMultiply(plbScale, plbPos);
-
-	m_boxOffset = m_shapes.AddGeometry(MODEL_TYPE_CUBE);
-	XMMATRIX boxScale = XMMatrixScaling(2.5f, 2.5f, 2.5f);
-	XMMATRIX boxOffset = XMMatrixTranslation(0.0f, 2.5f, 0.0f);
-	m_boxWorld = XMMatrixMultiply(boxScale, boxOffset);
-
-	m_sphereOffset = m_shapes.AddGeometry(MODEL_TYPE_SPHERE);
-	XMMATRIX sphereScale = XMMatrixScaling(2.0f, 2.0f, 2.0f);
-	XMMATRIX sphereOffset = XMMatrixTranslation(0.0f, 2.0f, 0.0f);
-	m_sphereWorld = XMMatrixMultiply(sphereScale, sphereOffset);
-
-	//m_skullOffset = m_shapes.AddCustomGeometry(L"Resources/skull.txt");
-	XMMATRIX skullScale = XMMatrixScaling(0.5f, 0.5f, 0.5f);
-	XMMATRIX skullOffset = XMMatrixTranslation(0.0f, 1.8f, 0.0f);
-	m_skullWorld = XMMatrixMultiply(skullScale, skullOffset);
-
-	//Material data
-	m_boxMaterial.Ambient = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
-	m_boxMaterial.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	m_boxMaterial.Specular = XMFLOAT4(0.6f, 0.6f, 0.6f, 16.0f);
-
-	m_sphereMaterial.Ambient = XMFLOAT4(0.5f, 0.5f, 0.1f, 1.0f);
-	m_sphereMaterial.Diffuse = XMFLOAT4(0.8f, 0.7f, 0.3f, 1.0f);
-	m_sphereMaterial.Specular = XMFLOAT4(0.5f, 0.5f, 0.5f, 2.0f);
-
-	m_cylinderMaterial.Ambient = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
-	m_cylinderMaterial.Diffuse = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
-	m_cylinderMaterial.Specular = XMFLOAT4(0.4f, 0.4f, 0.4f, 16.0f);
-
-	m_plainMaterial.Ambient = XMFLOAT4(0.48, 0.77f, 0.46f, 1.0f);
-	m_plainMaterial.Diffuse = XMFLOAT4(0.48, 0.77f, 0.46f, 1.0f);
-	m_plainMaterial.Specular = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
-
-	m_simpleMaterial.Ambient = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
-	m_simpleMaterial.Diffuse = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
-	m_simpleMaterial.Specular = XMFLOAT4(1.0f, 1.0f, 1.0f, 10.0f);
-
-	//light data
-	m_basicLight.Directional.Ambient = XMFLOAT4(0.5f, 0.45f, 0.4f, 1.0f);
-	m_basicLight.Directional.Diffuse = XMFLOAT4(0.95f, 0.90f, 0.85f, 1.0f);
+	//-----------------------------------------------------------------------------------------------------
+	//        Light Init
+	//-----------------------------------------------------------------------------------------------------
+	m_basicLight.Directional.Ambient = XMFLOAT4(0.6f, 0.5f, 0.4f, 1.0f);
+	m_basicLight.Directional.Diffuse = XMFLOAT4(1.0f, 0.95f, 0.85f, 1.0f);
 	m_basicLight.Directional.Specular = XMFLOAT4(0.5f, 0.5f, 0.35f, 1.0f);
 	m_basicLight.Directional.Direction = XMFLOAT3(-0.707f, -1.0f, 0.7f);
+	
 	/*
 	m_basicLight.Spot.Ambient = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
 	m_basicLight.Spot.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -219,53 +171,89 @@ bool SimpleApp::Init()
 	*/
 	/*
 	m_basicLight.Point.Ambient = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
-	m_basicLight.Point.Diffuse = XMFLOAT4(0.0f, 0.1f, 1.0f, 1.0f);
+	m_basicLight.Point.Diffuse = XMFLOAT4(0.0f, 0.0f, 0.8f, 1.0f);
 	m_basicLight.Point.Specular =XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
 	m_basicLight.Point.Att = XMFLOAT3(1.0f, 0.1f, 0.01f);
-	m_basicLight.Point.Range = 500.0f;
+	m_basicLight.Point.Range = 200.0f;
 	m_basicLight.Point.Position = XMFLOAT3(m_pointBulbPos);
 	*/
-	for (int i = 0; i < 10; ++i)
-	{
-		m_spheresOffset[i] = m_shapes.AddGeometry(MODEL_TYPE_SPHERE);
-		m_cylinderOffset[i] = m_shapes.AddGeometry(MODEL_TYPE_CYLINDER);
-	}
+
+	//-----------------------------------------------------------------------------------------------------
+	//        Material Init
+	//-----------------------------------------------------------------------------------------------------
+	m_matShiny.Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_matShiny.Diffuse = XMFLOAT4(0.9f, 0.9f, 0.9f, 1.0f);
+	m_matShiny.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 20.0f);
+
+	m_matRough.Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_matRough.Diffuse = XMFLOAT4(0.9f, 0.9f, 0.9f, 1.0f);
+	m_matRough.Specular = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	m_matWater.Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_matWater.Diffuse = XMFLOAT4(0.9f, 0.9f, 0.9f, 1.0f);
+	m_matWater.Specular = XMFLOAT4(0.3f, 0.3f, 0.3f, 10.0f);
+
+	//-----------------------------------------------------------------------------------------------------
+	//        Object Geometry Init
+	//-----------------------------------------------------------------------------------------------------
+
+	m_offsetPlain = m_shapes.AddGeometry(MODEL_TYPE_PLAIN);
 
 	for (int i = 0; i < 5; ++i)
 	{
-		m_cylinderWorld[i * 2 + 0] = XMMatrixTranslation(-5.0f, 1.5f, -10.0f + i*5.0f);
-		m_cylinderWorld[i * 2 + 1] = XMMatrixTranslation(+5.0f, 1.5f, -10.0f + i*5.0f);
-
-		m_spheresWorld[i * 2 + 0] = XMMatrixTranslation(-5.0f, 3.5f, -10.0f + i*5.0f);
-		m_spheresWorld[i * 2 + 1] = XMMatrixTranslation(+5.0f, 3.5f, -10.0f + i*5.0f);
+		m_offsetCont[i] = m_shapes.AddGeometry(MODEL_TYPE_CUBE);
 	}
-	
+
+	m_offsetWater = m_shapes.AddGeometry(MODEL_TYPE_PLAIN);
+
+	//-----------------------------------------------------------------------------------------------------
+	//        Object World Init
+	//-----------------------------------------------------------------------------------------------------
+
+	m_worldPlain = XMMatrixMultiply(XMMatrixScaling(30.0f, 1.0f, 30.0f), XMMatrixTranslation(0.0f, 0.0f, 0.0f));
+	m_worldCont[0] = XMMatrixMultiply(XMMatrixScaling(5.0f, 2.0f, 5.0f), XMMatrixTranslation(0.0f, 2.0f, -7.0f));
+	m_worldCont[1] = XMMatrixMultiply(XMMatrixScaling(1.0f, 2.0f, 5.0f), XMMatrixTranslation(2.5f, 4.0f, -7.0f));
+	m_worldCont[2] = XMMatrixMultiply(XMMatrixScaling(1.0f, 2.0f, 5.0f), XMMatrixTranslation(-2.5f, 4.0f, -7.0f));
+	m_worldCont[3] = XMMatrixMultiply(XMMatrixScaling(4.0f, 2.0f, 1.0f), XMMatrixTranslation(0.0f, 4.0f, -4.5f));
+	m_worldCont[4] = XMMatrixMultiply(XMMatrixScaling(4.0f, 2.0f, 1.0f), XMMatrixTranslation(0.0f, 4.0f, -9.5f));
+	m_worldWater = XMMatrixMultiply(XMMatrixScaling(0.2f, 1.0f, 0.2f), XMMatrixTranslation(0.0f, 4.5f, -7.0f));
+
+	//-----------------------------------------------------------------------------------------------------
+	//        Renderer Init
+	//-----------------------------------------------------------------------------------------------------
 	if (!m_shapes.Initialize(m_d3dDevice))
 	{
 		return false;
 	}
 
-	if (!m_colorShader.Initialize(m_d3dDevice, m_hWnd, m_plainMaterial))
+	if (!m_rendererShiny.Initialize(m_d3dDevice, m_hWnd, m_matShiny))
 	{
 		return false;
 	}
 
-	if (!m_lightShader.Initialize(m_d3dDevice, m_hWnd, m_simpleMaterial))
+	if (!m_rendererRough.Initialize(m_d3dDevice, m_hWnd, m_matRough))
 	{
 		return false;
 	}
 
-	if (!m_cylinderShader.Initialize(m_d3dDevice, m_hWnd, m_cylinderMaterial))
+	if (!m_rendererWater.Initialize(m_d3dDevice, m_hWnd, m_matWater))
 	{
 		return false;
 	}
 
-	if (!m_sphereShader.Initialize(m_d3dDevice, m_hWnd, m_sphereMaterial))
+	return true;
+}
+
+bool SimpleApp::Init()
+{
+	//Engine Init
+	if (!BaseApp::Init())
 	{
 		return false;
 	}
-	
-	if (!m_boxShader.Initialize(m_d3dDevice, m_hWnd, m_boxMaterial))
+
+	//Additional init
+	if (!SceneInit())
 	{
 		return false;
 	}
@@ -308,11 +296,11 @@ void SimpleApp::Update(float dt)
 	XMStoreFloat3(&m_basicLight.Spot.Direction, XMVector3Normalize(target - pos));
 	//m_basicLight.Spot.Direction = m_camera.GetRotation();
 	
-	m_grassX += 0.05f * dt;
-	m_grassY += 0.01f * dt;
-	XMMATRIX grassOffset = XMMatrixTranslation(m_grassX, m_grassY, 0.0f);
-	XMMATRIX grassScale = XMMatrixScaling(5.0f, 5.0f, 0.0f);
-	m_grassTexTransf = XMMatrixMultiply(grassScale, grassOffset);
+	m_waterX += 0.05f * dt;
+	m_waterY += 0.05f * dt;
+	XMMATRIX grassOffset = XMMatrixTranslation(m_waterX, m_waterY, 0.0f);
+	XMMATRIX grassScale = XMMatrixScaling(2.0f, 2.0f, 0.0f);
+	m_texTransfWater = XMMatrixMultiply(grassScale, grassOffset);
 }
 
 void SimpleApp::Draw()
@@ -327,35 +315,21 @@ void SimpleApp::Draw()
 	m_d3dDeviceContext->ClearDepthStencilView(m_depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
 
 	XMMATRIX m_view;
-
 	m_camera.Render();
 	m_camera.GetViewMatrix(m_view);
+	m_shapes.Render(m_d3dDeviceContext);	
 
-	m_shapes.Render(m_d3dDeviceContext);
+	//----------Rendering----------------------------------------------------------------------
+	m_rendererRough.Render(m_d3dDeviceContext, m_worldPlain, m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_texGrass, m_texTransfGrass, m_offsetPlain.indexCount, m_offsetPlain.indexOffset, m_offsetPlain.vertexOffset);
 
-	//Render in wireFrame
-	//m_d3dDeviceContext->RSSetState(m_wireFrameRS);
-	
-	
-	//render plain
-	m_colorShader.Render(m_d3dDeviceContext, m_plainWorld, m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_grassTexture, m_grassTexTransf, m_plainOffset.indexCount, m_plainOffset.indexOffset, m_plainOffset.vertexOffset);
-	//render box
-	m_boxShader.Render(m_d3dDeviceContext, m_boxWorld, m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_crateTexture, m_defTexTranf, m_boxOffset.indexCount, m_boxOffset.indexOffset, m_boxOffset.vertexOffset);
-	//render Sphere
-	//m_colorShader.Render(m_d3dDeviceContext, m_sphereWorld, m_view, m_projectionMatrix, m_crateTexture, m_sphereOffset.indexCount, m_sphereOffset.indexOffset, m_sphereOffset.vertexOffset);
-
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
-		//render 10 of each: Spheres and Cylinders
-		m_sphereShader.Render(m_d3dDeviceContext, m_spheresWorld[i], m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_crateTexture, m_defTexTranf, m_spheresOffset[i].indexCount, m_spheresOffset[i].indexOffset, m_spheresOffset[i].vertexOffset);
-		m_cylinderShader.Render(m_d3dDeviceContext, m_cylinderWorld[i], m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_crateTexture, m_defTexTranf, m_cylinderOffset[i].indexCount, m_cylinderOffset[i].indexOffset, m_cylinderOffset[i].vertexOffset);
+		m_rendererShiny.Render(m_d3dDeviceContext, m_worldCont[i], m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_texBrick, m_texTransfBrick, m_offsetCont[i].indexCount, m_offsetCont[i].indexOffset, m_offsetCont[i].vertexOffset);
 	}
-	
-	m_boxShader.Render(m_d3dDeviceContext, m_spheresWorld[10], m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_crateTexture, m_defTexTranf, m_spheresOffset[10].indexCount, m_spheresOffset[10].indexOffset, m_spheresOffset[10].vertexOffset);
 
-	//m_d3dDeviceContext->RSSetState(m_solidRS);
-	//m_lightShader.Render(m_d3dDeviceContext, m_skullWorld, m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_crateTexture, m_skullOffset.indexCount, m_skullOffset.indexOffset, m_skullOffset.vertexOffset);
-	
+	m_rendererWater.Render(m_d3dDeviceContext, m_worldWater, m_view, m_projectionMatrix, m_basicLight, m_camera.GetPosition(), m_texWater, m_texTransfWater, m_offsetWater.indexCount, m_offsetWater.indexOffset, m_offsetWater.vertexOffset);
+
+	//-----------------------------------------------------------------------------------------
 
 	if (VSYNC_ENABLED)
 	{
@@ -387,8 +361,8 @@ void SimpleApp::OnMouseMove(WPARAM btnState, int x, int y)
 		float dx = XMConvertToRadians(static_cast<float>(x - m_lastMousePos.x));
 		float dy = XMConvertToRadians(static_cast<float>(y - m_lastMousePos.y));
 
-		m_theta -= dx;
-		m_radius += dy;
+		m_theta -= dx*2.2f;
+		m_radius += dy*2.2f;
 	}
 	else if( (btnState & MK_RBUTTON) != 0)
 	{
